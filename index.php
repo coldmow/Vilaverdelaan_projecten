ca2829e1749b5f42a89a55b20af22f00806cdeb3
coldmow <mohammedb060@gmail.com>
2016-12-01 09:53:45 +0100

    Added a javascript 'register user' function to learn about objects and classes in js

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<h1>hello world</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+
+<body>
+	
+	<div class="registreer">
+		<label for="">Gebruikersnaam</label>
+		<input type="text">
+		<br>
+		<label for="">Wachtwoord</label>
+		<input type="text">
+	</div>
+	
+	
+	<?php
+
+	?>
+	<script>
+
+		function newUser(gebruikersnaam,wachtwoord){
+			this.usernames = [];
+			this.passwords = [];
+			this.toevoegen = function(gebruikersnaam,wachtwoord){
+				this.usernames.push(gebruikersnaam);
+			};
+		}
+
+		document.querySelector("selectors: DOMString")
+	</script>
+</body>
+
+</html>
